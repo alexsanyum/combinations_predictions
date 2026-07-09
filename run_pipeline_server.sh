00d760370bdc4852dca46fd7d72cdecef279306b
alexsanyum <sanyumale@gmail.com>
2026-07-09 17:10:32 -0300

    Change partition to sequana_gpu_dev in job script

--- run_pipeline_server.sh
+++ run_pipeline_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=embs_generation
 #SBATCH --nodes=1
-#SBATCH --partition=sequana_gpu       
+#SBATCH --partition=sequana_gpu_dev       
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1                  
 #SBATCH --time=00:10:00
