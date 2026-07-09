1e1fff3b4360cac83004d99a8a634eb3163cfc52
alexsanyum <sanyumale@gmail.com>
2026-07-09 15:44:15 -0300

    Fix script to run with singularity on submitted jobs

--- run_pipeline_server.sh
+++ run_pipeline_server.sh
@@ -8,27 +8,25 @@
 #SBATCH --output=/scratch/pcmrnbio2/alex.yumbo/logs/job_%j.out
 #SBATCH --error=/scratch/pcmrnbio2/alex.yumbo/logs/job_%j.err
 
-SCRATCH_DIR="/scratch/pcmrnbio2/$(whoami)"
-REPO_DIR="/prj/pcmrnbio2/alex.yumbo/combinations_predictions"
-# Path to containers 
-CONTAINER_DATA="$SCRATCH_DIR/containers/py_data.sif"
-CONTAINER_EMBS="$SCRATCH_DIR/containers/py_embs.sif"
-CONTAINER_ML="$SCRATCH_DIR/containers/py_ml.sif"
-
 
 cd $SLURM_SUBMIT_DIR
-echo "Directory: $SLURM_SUBMIT_DIR"
+pwd
 
-# 
-echo "Test if singularity is working..."
+SCRATCH_DIR="/scratch/pcmrnbio2/$(whoami)"
+REPO_DIR="/prj/pcmrnbio2/alex.yumbo/combinations_predictions"
 
+# Mount the scratch directory to the container and run the Python script
 
-singularity exec \
-    --bind $REPO_DIR:/app \
-    --bind $SCRATCH_DIR:$SCRATCH_DIR \
-    $CONTAINER_EMBS \
-    python --version
+# - Montamos o repositório do /prj dentro da pasta /app do container
+# - Usamos --pwd /app para forçar o Python a rodar de dentro do seu código
 
+export RUN_EMBS="singularity exec --nv --bind $REPO_DIR:/app --bind $SCRATCH_DIR:$SCRATCH_DIR --pwd /app $SCRATCH_DIR/containers/py_embs.sif"
+
+${RUN_EMBS} python src/smiles2clsembs.py \
+    --smiles_path $SCRATCH_DIR/data/normalized_abx.csv \
+    --smiles_column normalized_smiles \
+    --model_name DeepChem/ChemBERTa-77M-MLM \
+    --output_path $SCRATCH_DIR/data/abx_embs.npz
 # singularity exec \
 #     --bind $REPO_DIR:/app \
 #     --bind $SCRATCH_DIR:$SCRATCH_DIR \
