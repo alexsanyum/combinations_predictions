06f55ac8f6576ed7846d06a560370681e2545bea
alexsanyum <sanyumale@gmail.com>
2026-07-09 15:18:15 -0300

    Debugging container for embs

--- run_pipeline_server.sh
+++ run_pipeline_server.sh
@@ -15,33 +15,41 @@ CONTAINER_DATA="$SCRATCH_DIR/containers/py_data.sif"
 CONTAINER_EMBS="$SCRATCH_DIR/containers/py_embs.sif"
 CONTAINER_ML="$SCRATCH_DIR/containers/py_ml.sif"
 
-
+echo "Current working directory: $(pwd)"
 cd $REPO_DIR
+echo "Changed working directory to: $(pwd)"
 
 # 
-echo "Generating embeddings for antibiotics..."
+echo "Test if singularity is working..."
+
 
 singularity exec \
     --bind $REPO_DIR:/app \
     --bind $SCRATCH_DIR:$SCRATCH_DIR \
     $CONTAINER_EMBS \
-    python src/smiles2clsembs.py \
-        --smiles_path $SCRATCH_DIR/data/normalized_abx.csv \
-        --smiles_column normalized_smiles \
-        --model_name DeepChem/ChemBERTa-77M-MLM \
-        --output_path $SCRATCH_DIR/data/abx_embs.npz
+    python --version
 
-echo "Generating embeddings for small molecules..."
+# singularity exec \
+#     --bind $REPO_DIR:/app \
+#     --bind $SCRATCH_DIR:$SCRATCH_DIR \
+#     $CONTAINER_EMBS \
+#     python src/smiles2clsembs.py \
+#         --smiles_path $SCRATCH_DIR/data/normalized_abx.csv \
+#         --smiles_column normalized_smiles \
+#         --model_name DeepChem/ChemBERTa-77M-MLM \
+#         --output_path $SCRATCH_DIR/data/abx_embs.npz
 
-singularity exec \
-    --bind $REPO_DIR:/app \
-    --bind $SCRATCH_DIR:$SCRATCH_DIR \
-    $CONTAINER_EMBS \
-    python src/smiles2clsembs.py \
-        --smiles_path $SCRATCH_DIR/data/normalized_small_mol.csv \
-        --smiles_column normalized_smiles \
-        --model_name DeepChem/ChemBERTa-77M-MLM \
-        --output_path $SCRATCH_DIR/data/small_mol_embs.npz
+# echo "Generating embeddings for small molecules..."
+
+# singularity exec \
+#     --bind $REPO_DIR:/app \
+#     --bind $SCRATCH_DIR:$SCRATCH_DIR \
+#     $CONTAINER_EMBS \
+#     python src/smiles2clsembs.py \
+#         --smiles_path $SCRATCH_DIR/data/normalized_small_mol.csv \
+#         --smiles_column normalized_smiles \
+#         --model_name DeepChem/ChemBERTa-77M-MLM \
+#         --output_path $SCRATCH_DIR/data/small_mol_embs.npz
 
 
 #      sequana_cpu_dev       1         1        4      192    00:20:00 
