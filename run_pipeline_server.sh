6c87a6f6ac0f9386ca7511234e275087a56c9b2a
alexsanyum <sanyumale@gmail.com>
2026-07-09 16:08:46 -0300

    Refactor submission to run only on scratch

--- run_pipeline_server.sh
+++ run_pipeline_server.sh
@@ -9,43 +9,13 @@
 #SBATCH --error=/scratch/pcmrnbio2/alex.yumbo/logs/job_%j.err
 
 
-# cd $SLURM_SUBMIT_DIR
-# pwd
+REPO_DIR="/scratch/pcmrnbio2/alex.yumbo/combinations_predictions"
+CONTAINER_IMG="/scratch/pcmrnbio2/alex.yumbo/containers/py_embs.sif"
 
-SCRATCH_DIR="/scratch/pcmrnbio2/$(whoami)"
-REPO_DIR="/prj/pcmrnbio2/alex.yumbo/combinations_predictions"
-
-# Mount the scratch directory to the container and run the Python script
-
-# - Montamos o repositório do /prj dentro da pasta /app do container
-# - Usamos --pwd /app para forçar o Python a rodar de dentro do seu código
-
-export RUN_EMBS="singularity exec --nv --bind $REPO_DIR:/app --bind $SCRATCH_DIR:$SCRATCH_DIR --pwd /app $SCRATCH_DIR/containers/py_embs.sif"
-
-${RUN_EMBS} python src/smiles2clsembs.py \
-    --smiles_path $SCRATCH_DIR/data/normalized_abx.csv \
+singularity exec --nv \
+    -B $REPO_DIR:/app \
+    $CONTAINER_IMG \
+    python /app/src/smiles2clsembs.py --smiles_path /app/data/normalized_abx.csv \
     --smiles_column normalized_smiles \
     --model_name DeepChem/ChemBERTa-77M-MLM \
-    --output_path $SCRATCH_DIR/data/abx_embs.npz
-# singularity exec \
-#     --bind $REPO_DIR:/app \
-#     --bind $SCRATCH_DIR:$SCRATCH_DIR \
-#     $CONTAINER_EMBS \
-#     python src/smiles2clsembs.py \
-#         --smiles_path $SCRATCH_DIR/data/normalized_abx.csv \
-#         --smiles_column normalized_smiles \
-#         --model_name DeepChem/ChemBERTa-77M-MLM \
-#         --output_path $SCRATCH_DIR/data/abx_embs.npz
-
-# echo "Generating embeddings for small molecules..."
-
-# singularity exec \
-#     --bind $REPO_DIR:/app \
-#     --bind $SCRATCH_DIR:$SCRATCH_DIR \
-#     $CONTAINER_EMBS \
-#     python src/smiles2clsembs.py \
-#         --smiles_path $SCRATCH_DIR/data/normalized_small_mol.csv \
-#         --smiles_column normalized_smiles \
-#         --model_name DeepChem/ChemBERTa-77M-MLM \
-#         --output_path $SCRATCH_DIR/data/small_mol_embs.npz
-
+    --output_path /app/data/abx_embs.npz
