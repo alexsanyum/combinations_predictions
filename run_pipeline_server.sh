48101f89b5d2a27ac7d2c4bf9e58c5db825019e0
alexsanyum <sanyumale@gmail.com>
2026-07-09 15:36:07 -0300

    Fix partition

--- run_pipeline_server.sh
+++ run_pipeline_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=embs_generation
 #SBATCH --nodes=1
-#SBATCH --partition=sequana_cpu_dev       # Obrigatoriamente sequana_gpu ou gpu
+#SBATCH --partition=sequana_gpu_dev       # Obrigatoriamente sequana_gpu ou gpu
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1                  # Solicita 1 GPU para o processo
 #SBATCH --time=00:10:00
