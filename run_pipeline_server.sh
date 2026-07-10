dd95c58db0e2f4c6a34a5ac31e9ea86f9574d2de
alexsanyum <sanyumale@gmail.com>
2026-07-10 09:53:26 -0300

    Reformulate script resources allocation

--- run_pipeline_server.sh
+++ run_pipeline_server.sh
@@ -2,9 +2,10 @@
 #SBATCH --job-name=concat_embs
 #SBATCH --nodes=1
 #SBATCH --partition=sequana_cpu_dev       
-#SBATCH --ntasks-per-node=16                  
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8               
 #SBATCH --time=00:10:00
-#SBATCH --mem=32G
+#SBATCH --mem=64G
 #SBATCH --chdir=/scratch/pcmrnbio2/alex.yumbo/combinations_predictions
 #SBATCH --output=/scratch/pcmrnbio2/alex.yumbo/logs/job_%j.out
 #SBATCH --error=/scratch/pcmrnbio2/alex.yumbo/logs/job_%j.err
@@ -12,20 +13,19 @@
 REPO_DIR="/scratch/pcmrnbio2/alex.yumbo/combinations_predictions"
 CONTAINER_IMG="/scratch/pcmrnbio2/alex.yumbo/containers/py_data.sif"
 
-echo "=== CONTAINER SANITY CHECK ==="
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OPENBLAS_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-# 1. Verify python path and version inside the container
-echo "Checking Python path inside container:"
-singularity exec $CONTAINER_IMG which python
 
-echo "Checking Python version inside container:"
-singularity exec $CONTAINER_IMG python --version
+echo "Test if is accessing to the repository..."
 
-# 2. Test running code and checking directory bindings
-echo "Testing repository access and Python execution:"
 singularity exec \
     -B $REPO_DIR:/app \
     $CONTAINER_IMG \
-    python -c "import os; print('Hello from the container!'); print('Available files in /app:', os.listdir('/app'))"
-
-echo "=== CHECK COMPLETE ==="
+    python /app/src/concat_strain_embs.py --comb_data /app/data/valid_comb_data.csv \
+                                      --norm_small_mols /app/data/normalized_small_mol.csv \
+                                      --small_mol_embs /app/data/small_mol_embs.npz \
+                                      --norm_abx /app/data/normalized_abx.csv \
+                                      --abx_embs /app/data/abx_embs.npz \
+                                      --output_dir /app/data/strains_embs/
