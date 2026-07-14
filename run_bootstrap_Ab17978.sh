fe4351acfe786e9c2d6375aafdb7acaee32863ed
alexsanyum <sanyumale@gmail.com>
2026-07-14 09:14:18 -0300

    Add bootstrap job submission script for strain analysis

--- run_bootstrap_Ab17978.sh
+++ run_bootstrap_Ab17978.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
-
-# Define all 6 strains to process
-STRAINS=("Ab17978" "AbLac4" "Kp0087" "Kp43816" "PAO1" "Pa0095")
-MODEL="lgbm" # Modify this if you are running other architectures!
-
-for STRAIN in "${STRAINS[@]}"; do
-    echo "Submitting bootstrap job for Strain: $STRAIN, Model: $MODEL"
-    
-    sbatch <<EOT
-#!/bin/bash
-#SBATCH --job-name=boot_${STRAIN}
+#SBATCH --job-name=boot_Ab17978
 #SBATCH --partition=sequana_cpu_dev
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=64G
 #SBATCH --time=00:20:00
 #SBATCH --chdir=/scratch/pcmrnbio2/alex.yumbo/combinations_predictions
-#SBATCH --output=/scratch/pcmrnbio2/alex.yumbo/logs/boot_dev_${STRAIN}_%j.out
-#SBATCH --error=/scratch/pcmrnbio2/alex.yumbo/logs/boot_dev_${STRAIN}_%j.err
+#SBATCH --output=/scratch/pcmrnbio2/alex.yumbo/logs/boot_dev_Ab17978_%j.out
+#SBATCH --error=/scratch/pcmrnbio2/alex.yumbo/logs/boot_dev_Ab17978_%j.err
+
+# Define all 6 strains to process
+MODEL="lgbm" # Modify this if you are running other architectures!
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
@@ -37,6 +29,3 @@ singularity exec -B \$REPO_DIR:/app \$CONTAINER_IMG \
         --STRAIN "$STRAIN" \
         --MODEL_NAME "$MODEL" \
         --N_BOOTSTRAPS 2
-EOT
-
-done
